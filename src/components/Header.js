e99ec82857a1e647b0610d7aec98325d99ec3dac
James Smith <jasmith.188@gmail.com>
2020-11-03 14:50:51 -0500

    update navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,19 +9,16 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <Nav.Link href="#link">Second Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#home">Prep List</Nav.Link>
+            <Nav.Link href="#link">Schedule</Nav.Link>
+            <Nav.Link href="#link">Another Link</Nav.Link>
+            <NavDropdown title="Order Guides" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Produce</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Red Meat/Poultry
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Seafood</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Dry Goods</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
